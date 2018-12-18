bbacb5e84ad6ea5b3664329b4e56c91909a09314
Jordan George <jordanngeorge@gmail.com>
2018-12-18 18:45:28 -0500

    mongo/mlab

--- index.js
+++ index.js
@@ -1,22 +1,52 @@
-#!/usr/bin/env node
+// Get environment variables
+require('dotenv').config()
 
 // Dependencies
-var express = require('express'),
-    app     = express(),
-    morgan  = require("morgan");
+var express         = require('express'),
+    app             = express(),
+    mongoose        = require("mongoose"), // allows communication between mongodb and express
+    bodyParser      = require("body-parser"), // pull information from HTML POST
+    morgan          = require("morgan"),
+    methodOverride  = require("method-override"),
+    seedDB          = require("./seeds");
 
-// App configuration
+// Express Configuration
+// Sets the connection to MongoDB
+var mongo_url = process.env.DATABASEURL || "mongodb://localhost/jordangeorge";
+mongoose.connect(mongo_url, { useNewUrlParser: true });
+
+// Logging and Parsing
+app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(morgan('dev')); // log with Morgan
+app.use(methodOverride("_method"));
+// seedDB(); // seed the database
 
 // Routes
+var Post = require("./models/post");
 app.get('/', function(req, res) {
-  res.render("blog")
+  Post.find({}, function(err, allPosts) {
+     if (err) {
+         console.log(err);
+     } else {
+        res.render("blog", {posts: allPosts});
+     }
+  });
 });
 
 app.get('/post/:id', function(req, res) {
-  res.render("post")
+  // find the Post with provided ID
+  Post.findById(req.params.id).populate("comments").exec(function(err, foundPost){
+      if(err){
+          console.log(err);
+      } else {
+          console.log(foundPost)
+          //render show template with that Post
+          res.render("post", {post: foundPost});
+      }
+  });
 });
 
 app.get('/resume', function(req, res) {
--- public/js/blogs.js
+++ public/js/blogs.js
@@ -1,2 +1,41 @@
-#!/usr/bin/env node
-console.log(`The value of BLOG_ID is ${process.env.BLOG_ID}`);
+container()
+
+function container() {
+  const blog_id = config.BLOG_ID;
+  const api_key = config.BLOGGER_SECRET_API_KEY;
+
+  var url = "https://www.googleapis.com/blogger/v3/blogs/" + blog_id + "/posts?key=" + api_key;
+
+  var HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+      var anHttpRequest = new XMLHttpRequest();
+      anHttpRequest.onreadystatechange = function() {
+        if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+        aCallback(anHttpRequest.responseText);
+      }
+
+      anHttpRequest.open("GET", aUrl, true );
+      anHttpRequest.send( null );
+    }
+  }
+
+  function handleResponse(response) {
+    var arr = response['items']
+    for (var i = 0; i < arr.length; i++) {
+      var obj = arr[i];
+      console.log(obj.content);
+      document.getElementById("blog-content").innerHTML += "<div class=\"post\"><h1><a href=\"/post/" + obj.id + "\">" + obj.title + "</a></h1>" + obj.content.substring(0, 600) + "...</div>";
+    }
+  }
+
+  var client = new HttpClient();
+  client.get(url, function(response) {
+    console.log(response);
+    response = JSON.parse(response);
+    if (response["items"] != undefined) {
+      handleResponse(response);
+    } else {
+      document.getElementById("content").innerHTML += "<div class=\"post\"><h1 style=\"text-align:center\">No posts to show.</h1></div>";
+    }
+  });
+}

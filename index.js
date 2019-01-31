ce1f55d78b69ff04d144a5bbb2ce4bec6dcacc79
Jordan George <jordanngeorge@gmail.com>
2019-01-30 21:19:48 -0500

    port

--- index.js
+++ index.js
@@ -35,4 +35,7 @@ var routes = require('./routes');
 app.use("/", routes);
 
 // Listening
-app.listen(process.env.PORT);
+var port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", function() {
+  console.log("Listening on Port 3000");
+});
